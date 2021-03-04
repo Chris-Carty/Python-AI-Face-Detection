9858ebd5f5e76db5358213d9f23d51ff7ddcd618
Chris-Carty <69872982+Chris-Carty@users.noreply.github.com>
2021-03-04 15:51:43 +0000

    Updates

--- Face_Detector.py
+++ Face_Detector.py
@@ -1,5 +1,25 @@
 import cv2
 
+# Load pre-trained data on face frontals from opencv (haar cascade algorithm)
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+# example image to detect faces in
+img = cv2.imread('group-test.jpg')
 
-print("Code Completed")
+# make image grayscale (due to limitations of trained data)
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# detect faces
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+#test: print(face_coordinates)
+
+# Draw Rectangle for faces
+for (x, y, w, h) in face_coordinates:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 3)
+
+# example image
+cv2.imshow('ai-face-detection', img)
+# keeps program running until key is pressed
+cv2.waitKey()
+
+print("Code Completed") 
